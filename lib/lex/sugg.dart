fc1b526d265d0483a8a7d8d6503a5144001ed6ed
Sakib <sakibul706@gmail.com>
2026-03-01 20:34:16 +0600

    sugg seperate thread

--- lib/lex/sugg.dart
+++ lib/lex/sugg.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
+import 'dart:isolate';
 
 import 'package:ara_dict/data.dart';
 import 'package:ara_dict/db.dart';
+import 'package:ara_dict/lex/sugg_cache.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -12,10 +15,11 @@ const int searchSuggestionsLimit = 10;
 const String suggDataSep = '#';
 
 class SearchSuggestions {
-  static final Map<String, SuggestionMeta> _suggMap = {};
-  static final List<String> _allRootKeys = [];
-  static final List<String> _allWordKeys = [];
-  static final Map<String, List<String>> _prefixIndex = {};
+  // static final Map<String, SuggestionMeta> _suggMap = {};
+  // static final List<String> _allRootKeys = [];
+  // static final List<String> _allWordKeys = [];
+  // static final Map<String, List<String>> _prefixIndex = {};
+  static var _datas = SuggDatas.empty();
 
   static bool _initialized = false;
 
@@ -27,133 +31,34 @@ class SearchSuggestions {
     return _initialized && appSettingsNotifier.showSearchSugg;
   }
 
-  static final _prefixMaxLen = 3;
-
   static Future<void> init() async {
     if (_initialized || !appSettingsNotifier.showSearchSugg) return;
-    if (await _loadCache()) {
-      _initialized = true;
-      return;
-    }
 
-    final Map<String, Set<String>> prefixIndexRootGen = {};
-    final Map<String, Set<String>> prefixIndexWordGen = {};
-    Stopwatch? sw;
-    if (kDebugMode) sw = Stopwatch()..start();
-    int totalPrefixses = 0;
-    int totalPrefixsActual = 0;
+    // _initialized = await _loadCache();
+    if (_initialized) return;
 
-    for (final d in allDictsExpeptArEn) {
-      final list = await DbService.getSearchSuggestionList(d);
+    final wordList = await Future.wait(
+      allDictsExpeptArEn.map(
+        (d) async => (d, await DbService.getSearchSuggestionList(d)),
+      ),
+    );
 
-      for (final (key, isRoot) in list) {
-        final existing = _suggMap[key];
-
-        if (existing != null && !existing.isRoot && isRoot) {
-          _suggMap[key] = SuggestionMeta(true, existing.dicts);
-          _allRootKeys.add(key);
-          _allWordKeys.remove(key);
-
-          for (int i = 1; i <= _prefixMaxLen && i <= key.length; i++) {
-            final prefix = key.substring(0, i);
-            final wBucket = prefixIndexWordGen[prefix] ?? <String>{};
-            if (wBucket.isNotEmpty) wBucket.remove(key);
-            prefixIndexWordGen[prefix] = wBucket;
-
-            final rBucket = prefixIndexRootGen[prefix] ?? <String>{};
-            rBucket.add(key);
-            prefixIndexRootGen[prefix] = rBucket;
-          }
-        } else if (existing == null) {
-          _suggMap[key] = SuggestionMeta(isRoot, {d});
-
-          if (isRoot) {
-            _allRootKeys.add(key);
-          } else {
-            _allWordKeys.add(key);
-          }
-
-          // build prefix index
-          for (int i = 1; i <= _prefixMaxLen && i <= key.length; i++) {
-            final prefix = key.substring(0, i);
-            bool added = false;
-            if (isRoot) {
-              final rBucket = prefixIndexRootGen[prefix] ?? <String>{};
-              added = rBucket.add(key);
-              prefixIndexRootGen[prefix] = rBucket;
-            } else {
-              final wBucket = prefixIndexWordGen[prefix] ?? <String>{};
-              added = wBucket.add(key);
-              prefixIndexWordGen[prefix] = wBucket;
-            }
-            if (added) totalPrefixses++;
-          }
-        } else {
-          existing.dicts.add(d);
-        }
-      }
-    }
-
-    for (final e in prefixIndexRootGen.entries) {
-      final add = e.value.take(searchSuggestionsLimit).toList();
-      totalPrefixsActual += add.length;
-      _prefixIndex[e.key] = add;
-    }
-
-    for (final e in prefixIndexWordGen.entries) {
-      final willTake =
-          searchSuggestionsLimit - (_prefixIndex[e.key]?.length ?? 0);
-
-      if (willTake <= 0) continue;
-
-      final add = e.value.take(willTake).toList();
-      totalPrefixsActual += add.length;
-      _prefixIndex[e.key] = add;
-    }
-
-    if (kDebugMode) {
-      debugPrint('Total words indexed for searchSuggesstion');
-      debugPrint('Total words indexed for _suggMap: ${_suggMap.length}');
-      debugPrint(
-        'Total words indexed for _allRootKeys: ${_allRootKeys.length}',
-      );
-      debugPrint(
-        'Total words indexed for _allWordKeys: ${_allWordKeys.length}',
-      );
-      debugPrint(
-        'Total words indexed for _all__Keys: ${_allWordKeys.length + _allRootKeys.length}',
-      );
-      debugPrint(
-        'Total words indexed for prefixIndexGen: ${_prefixIndex.length} -> $totalPrefixses',
-      );
-      debugPrint(
-        'Total words indexed for _prefixIndex: ${_prefixIndex.length} -> $totalPrefixsActual',
-      );
-      debugPrint(
-        'Total words indexed for _prefixIndex diff: ${totalPrefixsActual - _prefixIndex.length}',
-      );
-
-      sw?.stop();
-      debugPrint('Took: ${sw?.elapsedMilliseconds}ms');
-
-      final sorted = List<String>.from(_allRootKeys)
-        ..sort((a, b) => b.length.compareTo(a.length));
-
-      // for (int i = 0; i < 50 && i < sorted.length; i++) {
-      //   debugPrint('$i. ${sorted[i].length} --> ${sorted[i]}');
-      // }
-
-      File(
-        '/tmp/soreted.txt',
-      ).writeAsString(sorted.map((i) => '${i.length} --> $i').join("\n"));
-      debugPrint('Biggest key -> ${sorted.first.length} --> ${sorted.first}');
-    }
+    // final list = await DbService.getSearchSuggestionList(d);
+    final res = await Isolate.run(() async {
+      return await initSuggetions(wordList);
+    });
+    if (res.isEmpty) return;
+    _datas = res;
     _initialized = true;
-    await saveToDb();
+
+    final cacheDir = await getApplicationCacheDirectory();
+    await Isolate.run(() async {
+      return await _saveCache(cacheDir.path, _datas);
+    });
   }
 
   static bool directMatch(String query, Dict d) {
-    return _suggMap[query]?.dicts.contains(d) ?? false;
+    return _datas.suggMap[query]?.dicts.contains(d) ?? false;
   }
 
   static Map<Dict, Set<String>> getSuggestions(String query) {
@@ -167,7 +72,7 @@ class SearchSuggestions {
       if (count >= limit) return;
       if (!addedWords.add(word)) return;
 
-      final sm = _suggMap[word];
+      final sm = _datas.suggMap[word];
       if (sm == null) return;
 
       for (final d in sm.dicts) {
@@ -178,14 +83,14 @@ class SearchSuggestions {
     }
 
     // Exact match
-    if (_suggMap.containsKey(query)) {
+    if (_datas.suggMap.containsKey(query)) {
       tryAdd(query);
     }
 
     if (count >= limit) return results;
 
     // Prefix match
-    final prefixList = _prefixIndex[query];
+    final prefixList = _datas.prefixIndex[query];
     if (prefixList != null) {
       for (final word in prefixList) {
         tryAdd(word);
@@ -196,7 +101,7 @@ class SearchSuggestions {
     if (count >= limit) return results;
 
     // Contains match
-    for (final word in _allRootKeys) {
+    for (final word in _datas.allRootKeys) {
       if (word.contains(query)) {
         tryAdd(word);
         if (count >= limit) break;
@@ -205,7 +110,7 @@ class SearchSuggestions {
 
     if (count >= limit) return results;
 
-    for (final word in _allWordKeys) {
+    for (final word in _datas.allWordKeys) {
       if (word.contains(query)) {
         tryAdd(word);
         if (count >= limit) break;
@@ -215,24 +120,16 @@ class SearchSuggestions {
     return results;
   }
 
-  static void _clearAll() {
-    _suggMap.clear();
-    _prefixIndex.clear();
-    _allRootKeys.clear();
-    _allWordKeys.clear();
-  }
-
   static const String _suggSaveFileName = 'sugg_data.txt';
   static const String _suggPrefixSaveFileName = 'sugg_prefix.txt';
 
-  static Future<void> saveToDb() async {
+  static Future<void> _saveCache(String cacheDir, SuggDatas currDatas) async {
     if (!_initialized) return;
-    final cacheDir = await getApplicationCacheDirectory();
 
     Stopwatch? sw;
     if (kDebugMode) sw = Stopwatch()..start();
 
-    final data = _suggMap.entries
+    final data = currDatas.suggMap.entries
         .map(
           (v) =>
               '${v.value.isRoot ? "1" : "0"}'
@@ -240,15 +137,13 @@ class SearchSuggestions {
         )
         .join('\n');
 
-    File(join(cacheDir.path, _suggSaveFileName)).writeAsString(data);
+    File(join(cacheDir, _suggSaveFileName)).writeAsString(data);
 
-    final prefixData = _prefixIndex.entries
+    final prefixData = currDatas.prefixIndex.entries
         .map((v) => '${v.key}$suggDataSep${jsonEncode(v.value)}')
         .join('\n');
 
-    File(
-      join(cacheDir.path, _suggPrefixSaveFileName),
-    ).writeAsString(prefixData);
+    File(join(cacheDir, _suggPrefixSaveFileName)).writeAsString(prefixData);
 
     if (kDebugMode) {
       debugPrint('sugg_db saved in ${sw?.elapsedMilliseconds}ms');
@@ -256,71 +151,39 @@ class SearchSuggestions {
   }
 
   static Future<bool> _loadCache() async {
-    Stopwatch? sw;
-    if (kDebugMode) sw = Stopwatch()..start();
-    _clearAll();
-
-    final cacheDir = await getApplicationCacheDirectory();
     try {
-      var file = File(join(cacheDir.path, _suggSaveFileName));
-      if (!await file.exists()) return false;
-      var lines = await file.readAsLines();
+      final cacheDir = await getApplicationCacheDirectory();
+      final suggData = await File(
+        join(cacheDir.path, _suggSaveFileName),
+      ).readAsLines();
+      final prefixData = await File(
+        join(cacheDir.path, _suggPrefixSaveFileName),
+      ).readAsLines();
 
-      for (final l in lines) {
-        final datas = l.split(suggDataSep);
-        if (datas.length != 3) {
-          if (kDebugMode) debugPrint('malformedline $l');
-          continue;
-        }
+      // Parse in background isolate
+      final parsed = await Isolate.run(() {
+        return parseCacheDatas(suggData, prefixData);
+      });
 
-        final key = datas[2];
-        final isRoot = datas[0] == '1';
-        final dictTables = int.parse(datas[1]);
+      if (parsed.suggMap.isEmpty || parsed.prefixIndex.isEmpty) return false;
 
-        _suggMap[key] = SuggestionMeta(isRoot, decode(dictTables));
-        if (isRoot) {
-          _allRootKeys.add(key);
-        } else {
-          _allWordKeys.add(key);
-        }
-      }
-
-      file = File(join(cacheDir.path, _suggPrefixSaveFileName));
-
-      lines = await file.readAsLines();
-
-      for (final l in lines) {
-        final datas = l.split(suggDataSep);
-        if (datas.length != 2) {
-          if (kDebugMode) debugPrint('malformedline $l');
-          continue;
-        }
-
-        final prefix = datas[0];
-        // fsw.start();
-        final words = List<String>.from(jsonDecode(datas[1]));
-        // fsw.stop();
-
-        _prefixIndex[prefix] = words;
-      }
+      _datas = parsed;
+      return true;
     } catch (e) {
-      if (kDebugMode) {
-        debugPrint('err: $e');
-      }
-      _clearAll();
       return false;
     }
-    if (_suggMap.isEmpty || _prefixIndex.isEmpty) {
-      _clearAll();
-      return false;
-    }
-    if (kDebugMode) {
-      sw?.stop();
-      debugPrint(
-        'sugg_db loaded from chache in ${sw?.elapsedMilliseconds}ms or ${sw?.elapsed.inSeconds}s',
-      );
-    }
-    return true;
+
+    // if (_suggMap.isEmpty || _prefixIndex.isEmpty) {
+    //   _clearAll();
+    //   return false;
+    // }
+    // if (kDebugMode) {
+    //   sw?.stop();
+    //   debugPrint(
+    //     'sugg_db loaded from chache in ${sw?.elapsedMilliseconds}ms or ${sw?.elapsed.inSeconds}s',
+    //   );
+    // }
+    // return true;
   }
 }
 
---
+++ lib/lex/sugg_cache.dart
@@ -0,0 +1,201 @@
+import 'dart:convert';
+
+import 'package:ara_dict/data.dart';
+import 'package:ara_dict/db.dart';
+import 'package:ara_dict/lex/sugg.dart';
+
+class SuggDatas {
+  final Map<String, SuggestionMeta> suggMap;
+  final List<String> allRootKeys;
+  final List<String> allWordKeys;
+  final Map<String, List<String>> prefixIndex;
+
+  SuggDatas({
+    required this.suggMap,
+    required this.allRootKeys,
+    required this.allWordKeys,
+    required this.prefixIndex,
+  });
+
+  void clearAll() {
+    suggMap.clear();
+    prefixIndex.clear();
+    allRootKeys.clear();
+    allWordKeys.clear();
+  }
+
+  bool get isEmpty {
+    return suggMap.isEmpty || prefixIndex.isEmpty;
+  }
+
+  bool get isNotEmpty {
+    return suggMap.isNotEmpty || prefixIndex.isNotEmpty;
+  }
+
+  static SuggDatas empty() {
+    return SuggDatas(
+      suggMap: {},
+      allRootKeys: [],
+      allWordKeys: [],
+      prefixIndex: {},
+    );
+  }
+}
+
+const int _prefixMaxLen = 3;
+
+Future<SuggDatas> initSuggetions(List<(Dict, List<(String, bool)>)> wordList) async {
+  final currData = SuggDatas.empty();
+  final Map<String, Set<String>> prefixIndexRootGen = {};
+  final Map<String, Set<String>> prefixIndexWordGen = {};
+  for (final (d, list) in wordList) {
+    for (final (key, isRoot) in list) {
+      final existing = currData.suggMap[key];
+
+      if (existing != null && !existing.isRoot && isRoot) {
+        currData.suggMap[key] = SuggestionMeta(true, existing.dicts);
+        currData.allRootKeys.add(key);
+        currData.allWordKeys.remove(key);
+
+        for (int i = 1; i <= _prefixMaxLen && i <= key.length; i++) {
+          final prefix = key.substring(0, i);
+          final wBucket = prefixIndexWordGen[prefix] ?? <String>{};
+          if (wBucket.isNotEmpty) wBucket.remove(key);
+          prefixIndexWordGen[prefix] = wBucket;
+
+          final rBucket = prefixIndexRootGen[prefix] ?? <String>{};
+          rBucket.add(key);
+          prefixIndexRootGen[prefix] = rBucket;
+        }
+      } else if (existing == null) {
+        currData.suggMap[key] = SuggestionMeta(isRoot, {d});
+
+        if (isRoot) {
+          currData.allRootKeys.add(key);
+        } else {
+          currData.allWordKeys.add(key);
+        }
+
+        // build prefix index
+        for (int i = 1; i <= _prefixMaxLen && i <= key.length; i++) {
+          final prefix = key.substring(0, i);
+          if (isRoot) {
+            final rBucket = prefixIndexRootGen[prefix] ?? <String>{};
+            rBucket.add(key);
+            prefixIndexRootGen[prefix] = rBucket;
+          } else {
+            final wBucket = prefixIndexWordGen[prefix] ?? <String>{};
+            wBucket.add(key);
+            prefixIndexWordGen[prefix] = wBucket;
+          }
+          // if (added) totalPrefixses++;
+        }
+      } else {
+        existing.dicts.add(d);
+      }
+    }
+  }
+
+  for (final e in prefixIndexRootGen.entries) {
+    final add = e.value.take(searchSuggestionsLimit).toList();
+    // totalPrefixsActual += add.length;
+    currData.prefixIndex[e.key] = add;
+  }
+
+  for (final e in prefixIndexWordGen.entries) {
+    final willTake =
+        searchSuggestionsLimit - (currData.prefixIndex[e.key]?.length ?? 0);
+
+    if (willTake <= 0) continue;
+
+    final add = e.value.take(willTake).toList();
+    // totalPrefixsActual += add.length;
+    currData.prefixIndex[e.key] = add;
+  }
+
+  // if (kDebugMode) {
+  //   debugPrint('Total words indexed for searchSuggesstion');
+  //   debugPrint(
+  //     'Total words indexed for currData.suggMap: ${currData.suggMap.length}',
+  //   );
+  //   debugPrint(
+  //     'Total words indexed for currData.allRootKeys: ${currData.allRootKeys.length}',
+  //   );
+  //   debugPrint(
+  //     'Total words indexed for currData.allWordKeys: ${currData.allWordKeys.length}',
+  //   );
+  //   debugPrint(
+  //     'Total words indexed for currData.allcurrData.currData.Keys: ${currData.allWordKeys.length + currData.allRootKeys.length}',
+  //   );
+  //   debugPrint(
+  //     'Total words indexed for prefixIndexGen: ${currData.prefixIndex.length} -> $totalPrefixses',
+  //   );
+  //   debugPrint(
+  //     'Total words indexed for currData.prefixIndex: ${currData.prefixIndex.length} -> $totalPrefixsActual',
+  //   );
+  //   debugPrint(
+  //     'Total words indexed for currData.prefixIndex diff: ${totalPrefixsActual - currData.prefixIndex.length}',
+  //   );
+
+  //   sw?.stop();
+  //   debugPrint('Took: ${sw?.elapsedMilliseconds}ms');
+
+  //   final sorted = List<String>.from(currData.allRootKeys)
+  //     ..sort((a, b) => b.length.compareTo(a.length));
+
+  //   // for (int i = 0; i < 50 && i < sorted.length; i++) {
+  //   //   debugPrint('$i. ${sorted[i].length} --> ${sorted[i]}');
+  //   // }
+
+  //   // File(
+  //   //   '/tmp/soreted.txt',
+  //   // ).writeAsString(sorted.map((i) => '${i.length} --> $i').join("\n"));
+  //   debugPrint('Biggest key -> ${sorted.first.length} --> ${sorted.first}');
+  // }
+  return currData;
+}
+
+SuggDatas parseCacheDatas(List<String> suggLines, List<String> prefixLines) {
+  final suggMap = <String, SuggestionMeta>{};
+  final allRootKeys = <String>[];
+  final allWordKeys = <String>[];
+  final prefixIndex = <String, List<String>>{};
+
+  for (final l in suggLines) {
+    if (l.isEmpty) continue;
+
+    final datas = l.split(suggDataSep);
+    if (datas.length != 3) continue;
+
+    final isRoot = datas[0] == '1';
+    final dictTables = int.parse(datas[1]);
+    final key = datas[2];
+
+    suggMap[key] = SuggestionMeta(isRoot, decode(dictTables));
+
+    if (isRoot) {
+      allRootKeys.add(key);
+    } else {
+      allWordKeys.add(key);
+    }
+  }
+
+  for (final l in prefixLines) {
+    if (l.isEmpty) continue;
+
+    final datas = l.split(suggDataSep);
+    if (datas.length != 2) continue;
+
+    final prefix = datas[0];
+    final words = List<String>.from(jsonDecode(datas[1]));
+
+    prefixIndex[prefix] = words;
+  }
+
+  return SuggDatas(
+    suggMap: suggMap,
+    allRootKeys: allRootKeys,
+    allWordKeys: allWordKeys,
+    prefixIndex: prefixIndex,
+  );
+}

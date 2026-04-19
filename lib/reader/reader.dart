c37eb27835ec82bbc46bb5f5a7778cc752cd146e
Sakib <sakibul706@gmail.com>
2026-04-19 20:19:11 +0600

    reader exit msg

--- lib/reader/reader.dart
+++ lib/reader/reader.dart
@@ -224,6 +224,7 @@ Future<void> exitReaderPage(BuildContext context) async {
         context,
         'Exit Reader',
         message: 'Go to reader input page?',
+        confirmText: 'Exit'
       ) ??
       false) {
     if (!context.mounted) return;

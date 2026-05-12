d4cb6cbfa4ad01772e66de4c08a9c88c7c837f26
Sakib <sakibul706@gmail.com>
2026-05-12 11:49:23 +0600

    scrap 1st run stuff

--- lib/first_run.dart
+++ lib/first_run.dart
@@ -1,112 +1,116 @@
-import 'package:ara_dict/data.dart';
+// import 'package:ara_dict/data.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
-const String overViewURL = 'https://wizsk.github.io/apps/arabic_lexicons.html#video';
+const String overViewURL =
+    'https://wizsk.github.io/apps/arabic_lexicons.html#video';
 
-bool firstRunPopupShown = false;
-bool shouldShowFirstPopup() => !firstRunPopupShown && appConf.firstRun;
+// bool firstRunPopupShown = false;
+// bool shouldShowFirstPopup() => !firstRunPopupShown && appConf.firstRun;
 
-Future<void> _showWatchVideoDialog(BuildContext context) async {
-  if (!shouldShowFirstPopup()) return;
+// Future<void> _showWatchVideoDialog(BuildContext context) async {
+//   if (!shouldShowFirstPopup()) return;
 
-  firstRunPopupShown = true;
+//   firstRunPopupShown = true;
 
-  bool watched = false;
-  // const msg =
-  //     '⚠️ Please watch the walkthrough video to know how to use the app\n\n'
-  //     'Tap "Open" to start watching';
+//   bool watched = false;
+//   // const msg =
+//   //     '⚠️ Please watch the walkthrough video to know how to use the app\n\n'
+//   //     'Tap "Open" to start watching';
 
-  const msg =
-      // 'Thanks for downloading the app.\n\n'
-      '⚠️ Please watch the walkthrough video first.\n\n'
-      'It only takes a moment and will save you time by showing exactly how to use the app properly.\n\n'
-      'Tap "Open" to start watching.';
+//   const msg =
+//       // 'Thanks for downloading the app.\n\n'
+//       '⚠️ Please watch the walkthrough video first.\n\n'
+//       'It only takes a moment and will save you time by showing exactly how to use the app properly.\n\n'
+//       'Tap "Open" to start watching.';
 
-  await showDialog(
-    barrierDismissible: false,
-    context: context,
-    builder: (ctx) {
-      return StatefulBuilder(
-        builder: (ctx, setState) {
-          return AlertDialog(
-            scrollable: true,
-            constraints: BoxConstraints(maxWidth: 600),
-            title: const Text('Welcome!'),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(msg),
+//   await showDialog(
+//     barrierDismissible: false,
+//     context: context,
+//     builder: (ctx) {
+//       return StatefulBuilder(
+//         builder: (ctx, setState) {
+//           return AlertDialog(
+//             scrollable: true,
+//             constraints: BoxConstraints(maxWidth: 600),
+//             title: const Text('Welcome!'),
+//             content: Column(
+//               mainAxisSize: MainAxisSize.min,
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 const Text(msg),
 
-                const SizedBox(height: 8),
+//                 const SizedBox(height: 8),
 
-                InkWell(
-                  onTap: () => setState(() => watched = !watched),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 12.0),
-                    // .copyWith(left: 8),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 18,
-                          height: 18,
-                          child: Checkbox(
-                            value: watched,
-                            onChanged: (val) {
-                              setState(() => watched = val ?? false);
-                            },
-                            materialTapTargetSize:
-                                MaterialTapTargetSize.shrinkWrap,
-                            visualDensity: VisualDensity.compact,
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        const Expanded(
-                          child: Text(
-                            'Yes, I have watched it',
-                            // style: TextStyle(fontSize: 14),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(ctx).pop();
-                  if (watched) appConf.saveFirstRun(false);
-                },
-                child: const Text('Close'),
-              ),
+//                 InkWell(
+//                   onTap: () => setState(() => watched = !watched),
+//                   child: Padding(
+//                     padding: const EdgeInsets.symmetric(vertical: 12.0),
+//                     // .copyWith(left: 8),
+//                     child: Row(
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       children: [
+//                         SizedBox(
+//                           width: 18,
+//                           height: 18,
+//                           child: Checkbox(
+//                             value: watched,
+//                             onChanged: (val) {
+//                               setState(() => watched = val ?? false);
+//                             },
+//                             materialTapTargetSize:
+//                                 MaterialTapTargetSize.shrinkWrap,
+//                             visualDensity: VisualDensity.compact,
+//                           ),
+//                         ),
+//                         const SizedBox(width: 8),
+//                         const Expanded(
+//                           child: Text(
+//                             'Yes, I have watched it',
+//                             // style: TextStyle(fontSize: 14),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             actions: [
+//               TextButton(
+//                 onPressed: () {
+//                   Navigator.of(ctx).pop();
+//                   if (watched) appConf.saveFirstRun(false);
+//                 },
+//                 child: const Text('Close'),
+//               ),
 
-              FilledButton(
-                onPressed: () {
-                  launchUrl(Uri.parse(overViewURL));
-                },
-                child: Text('Open'),
-              ),
-            ],
-          );
-        },
-      );
-    },
-  );
-}
+//               FilledButton(
+//                 onPressed: () {
+//                   launchUrl(Uri.parse(overViewURL));
+//                 },
+//                 child: Text('Open'),
+//               ),
+//             ],
+//           );
+//         },
+//       );
+//     },
+//   );
+// }
 
-Future<void> showFirstRunPopup(BuildContext context) async {
-  if (!shouldShowFirstPopup()) return;
-  return _showWatchVideoDialog(context);
-}
+Future<void> showFirstRunPopup(BuildContext context) async {}
 
-void showFirstRunPopupPostFrame(BuildContext context) {
-  if (!shouldShowFirstPopup()) return;
+// Future<void> showFirstRunPopup(BuildContext context) async {
+//   if (!shouldShowFirstPopup()) return;
+//   return _showWatchVideoDialog(context);
+// }
 
-  WidgetsBinding.instance.addPostFrameCallback((_) async {
-    showFirstRunPopup(context);
-  });
-}
+void showFirstRunPopupPostFrame(BuildContext context) {}
+// void showFirstRunPopupPostFrame(BuildContext context) {
+//   if (!shouldShowFirstPopup()) return;
+
+//   WidgetsBinding.instance.addPostFrameCallback((_) async {
+//     showFirstRunPopup(context);
+//   });
+// }

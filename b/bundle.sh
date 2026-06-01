4454cedca54dd560164ffc80390bc5925ca2918b
Sakib <sakibul706@gmail.com>
2026-06-02 05:38:21 +0600

    edit b/bundle.sh

--- b/bundle.sh
+++ b/bundle.sh
@@ -5,7 +5,7 @@ source ./b/confirm.sh
 
 set -ex
 
-flutter build apk --release \
+flutter build bundle --release \
   --dart-define=APP_VERSION="$ver" \
   --dart-define=BUILD_UNIX_TIME=$(date +%s) \
   --dart-define=GIT_COMMIT="$gc" \
